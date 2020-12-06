07fe9414ac224d1ebc3a551297a43075549b6952
Jacques56100 <Jacques56100>
2020-12-06 11:16:23 +0100

    Ajout du championnat de France

--- public_html/script.js
+++ public_html/script.js
@@ -19,7 +19,10 @@ let content = "";
 
 
 // Les fonctions pour python
+let initPyDone = false;
 function initPy() {
+    if(initPyDone) return;
+    initPyDone = true;
     pyEditor = CodeMirror.fromTextArea(pyReply, {
         mode: 'text/x-python',
         viewportMargin: Infinity,
@@ -57,7 +60,10 @@ function execPy(prog) {
             });
 }
 
+let initSqlDone = false;
 function initSql() {
+    if(initSqlDone) return;
+    initSqlDone = true;
     dbEditor = CodeMirror.fromTextArea(dbReply, {
         mode: 'text/x-mysql',
         viewportMargin: Infinity,
@@ -142,7 +148,7 @@ function giveup() {
 }
 function getSolution() {
     if (question.getElementsByClassName("response").length != 0) {
-        return question.getElementsByClassName("response")[0].innerHTML;
+        return question.getElementsByClassName("response")[0].innerHTML.replaceAll("&lt;","<").replaceAll("&gt;",">");
     }
     return "";
 }
@@ -246,7 +252,6 @@ function openSqlFile(event) {
 }
 function reloadHtmlFile() {
     let start = document.getElementById("start");
-    let child = start.nextSibling;
     while(start.nextSibling != null) 
         sujet.removeChild(start.nextSibling);
     sujet.innerHTML += content;
@@ -271,6 +276,9 @@ function nextLoad() {
         fileName = loads.shift();
         fileExtension = fileName.split('.').pop();
         switch (fileExtension) {
+            case "py":
+                loadPy(fileName);
+                break;
             case "html":
                 loadHtml(fileName);
                 break;
@@ -336,6 +344,18 @@ function loadDb(dbFile) {
     };
     xdb.send(null);
 }
+function loadPy(pyFile) {
+    let xdb = new XMLHttpRequest();
+    xdb.overrideMimeType("text/python");
+    xdb.open('GET', "TP/" + pyFile, true);
+    xdb.onreadystatechange = function () {
+        if (xdb.readyState == 4 && xdb.status == "200") {
+            pyProg = xdb.responseText;
+            nextLoad();
+        }
+    };
+    xdb.send(null);
+}
 
 let regex = new RegExp('(\\?|&|^)html=(.*?)(&|$)');
 let html = document.location.href.match(regex);
@@ -354,6 +374,7 @@ if (py != null) {
     load("python-mode.js");
     load("skulpt.min.js");
     load("skulpt-stdlib.js");
+    if (py[2] != "") load(py[2] + ".py");
 }
 
 nextLoad();
